98aafe490f90480a65aade28e7d44036b737e98a
Vitoria Caroline <toriacf2015@gmail.com>
2020-10-30 14:56:30 -0300

    modificacao de arquivo

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let i = 0;
+//teste VERSIONAMENTO
+var i = 0;
+var number = 6;
   while (i <= 10) {
     let text = "The number is " + i;
     console.log(text);
+    console.log(number);
     i++
   } 
-
+//ISTO E UM TESTE VERSIONAMENTO
 
 
